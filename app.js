578f16817a9a33fd9aa67dd85304288957b4d7e7
Sutton <asutton4@una.edu>
2023-08-29 13:07:00 -0500

    bruh

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const port = (process.env.PORT || 3000)
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
 })
 
 
-app.listen(3000)
+app.listen(port, () => console.log('Server is running ... on' ${port} ));
