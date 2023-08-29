fc8445db1de7197ed6f7ab3a9df26081f0361923
Sutton <asutton4@una.edu>
2023-08-29 13:34:50 -0500

    bruh

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 const port = (process.env.PORT || 3000)
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
 })
 
-app.get('/whaterver', function(req,res) {
-  res.sendFile('index.html');
+app.get('/asutton4', function(req,res) {
+  res.render(__dirname + '/index.ejs');
+})
+
+app.get('/whatever', function(req,res) {
+  res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(port, () => console.log(`Server is running ... on ${port} `));
