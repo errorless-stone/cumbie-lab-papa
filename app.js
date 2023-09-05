1f00a6bc165237c07f989e574a9cfb5ce84ad781
Andrew Sutton <97402701+errorless-stone@users.noreply.github.com>
2023-09-05 12:38:14 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = (process.env.PORT || 3000)
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+4
 let myVariableServer = 'soft coded server data';
+
+app.get('/barry', function (req, res) {
+  res.render('index', 
+  {
+    'myVariableClient' : myVariableServer 
+  }
+  );
+})
+
+app.post('/postClientData', function (req, res) {
+  
+   console.log("body: ", req.body)
+   console.log("user Name: ", req.body.userName)
+  //  console.log("params: ", req.params['userName']);
+  
+  // myVariableServer = req.body.userName;
+
+  res.render('index', 
+  {
+    'myVariableClient' : req.body.userName 
+  }
+  );
+})
+
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
 })
 
-app.get('/asutton4', function(req,res) {
-  res.render(__dirname + '/index.ejs',
-  {
-    'myVariableClient' : myVariableServer
-  });
-})
-
-app.get('/whatever', function(req,res) {
+app.get('/whatever', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(port, () => console.log(`Server is running ... on ${port} `));
+
+
+// app.listen(3000)
+
+app.listen(port, () => console.log(`Server is running...on ${ port }` ));
